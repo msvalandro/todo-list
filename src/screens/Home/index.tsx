1c14f7edc9b29b4e7760f97144fbf0b9540af9ea
msvalandro <msvalandro@proton.me>
2024-07-14 13:01:55 +0100

    feat: create home screen

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+import { styles } from './styles'
+
+export function Home() {
+  return (
+    <View style={styles.header}>
+      <Text>hello world</Text>
+    </View>
+  )
+}
