5809a8a621eca7783111ea46e8ecf1fe2ede3716
msvalandro <msvalandro@proton.me>
2024-07-14 13:36:53 +0100

    feat: create input field

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,32 @@
-import { Text, View } from 'react-native'
+import { PlusCircle } from 'phosphor-react-native'
+import { Text, TextInput, TouchableOpacity, View } from 'react-native'
 
 import { Header } from '../../components/Header'
 import { styles } from './styles'
 
 export function Home() {
-  return <Header />
+  return (
+    <>
+      <Header />
+
+      <View style={styles.listContainer}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Adicione uma nova tarefa"
+            placeholderTextColor="#808080"
+          />
+
+          <TouchableOpacity style={styles.addButton}>
+            <PlusCircle size={16} color="#f2f2f2" />
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.listHeader}>
+          <Text>Criadas</Text>
+          <Text>Concluídas</Text>
+        </View>
+      </View>
+    </>
+  )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,8 +1,38 @@
 import { StyleSheet } from 'react-native'
 
 export const styles = StyleSheet.create({
-  header: {
-    height: 173,
-    backgroundColor: '#0D0D0D',
+  inputContainer: {
+    height: 52,
+    paddingHorizontal: 24,
+    marginTop: -26,
+
+    flexDirection: 'row',
   },
+  input: {
+    paddingHorizontal: 16,
+    marginRight: 4,
+    backgroundColor: '#262626',
+    borderWidth: 1,
+    borderColor: '#0D0D0D',
+    borderRadius: 6,
+
+    fontSize: 16,
+
+    flex: 1,
+  },
+  addButton: {
+    height: 52,
+    width: 52,
+    backgroundColor: '#1E6F9F',
+    borderRadius: 6,
+
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  listContainer: {
+    flex: 1,
+    backgroundColor: '#1A1A1A',
+    marginTop: -26,
+  },
+  listHeader: {},
 })
